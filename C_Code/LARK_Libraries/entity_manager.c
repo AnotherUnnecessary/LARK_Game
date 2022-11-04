eb5305de3fafcb6dca0192b09f782ed66e5a7030
AnotherUnnecessary <75258922+AnotherUnnecessary@users.noreply.github.com>
2022-11-03 23:14:49 -0400

    Added more things to level 1

--- C_Code/LARK_Libraries/entity_manager.c
+++ C_Code/LARK_Libraries/entity_manager.c
@@ -1,18 +1,31 @@
 #include "entity_manager.h"
 
 char scrollText1[512] = {
-    "\r\nThis is a scroll. They contain useful information to learn how to play the game.\r\nYou already know that if you walk over something, you can use './pickup.sh' to pick it up\r\nYou can also use './drop.sh' to drop it\r\nThere is also './use.sh' to use (but you already know that)"
+    "\r\n'I dont know how I got here.\r\nEverything is overwhelmingly dark or blindinly bright.\r\nIm scared but there must be a way out.'\r\nThe rest of the writing is illegible."
+};
+
+char scrollText2[512] = {
+    "\r\n'Its been days.\r\nThis L holds so much more value than any other L Ive ever seen\r\nI can see places just thinking about my sorroundings.\r\nls is the way'\r\nThere seems to be a drawing at the bottom.\r\nIt looks like a weird penguin."
 };
 
 char susText[24] = {"You are a sussy baka"};
 
 Transform *ROOT;
+//level1
 Transform *trigger1lvl1WakeUp;
+Transform *trigger2lvl1LRoom;
+Transform *trigger3lvl1RightRoom;
 Transform *key1lvl1;
+Transform *key2lvl1;
 Transform *door1lvl1;
+Transform *door2lvl1;
+Transform *door5lvl1;
+Transform *scroll1lvl1;
+Transform *scroll2lvl1;
+
+
 Transform *entity2;
 Transform *entity3;
-Transform *scroll1;
 Transform *lantern;
 Transform *chest;
 
@@ -37,7 +50,7 @@ void ENTITY_SETUP(){
 
     //TRUE LEVEL 1 TEST
 
-    //Trigger for the player waking up
+        //Trigger for the player waking up
     trigger1lvl1WakeUp = ADD_ENTITY();
     trigger1lvl1WakeUp->isFile = false;
     trigger1lvl1WakeUp->sprite = ' ';
@@ -48,8 +61,31 @@ void ENTITY_SETUP(){
     trigger1lvl1WakeUp->tag = DEFAULT;
     trigger1lvl1WakeUp->OnUpdate = &TriggerUpdate;
     sprintf(trigger1lvl1WakeUp->name, "You're finally awake. Its a cozy corner to wake up in.");
+        //L room lol
+    trigger2lvl1LRoom = ADD_ENTITY();
+    trigger2lvl1LRoom->isFile = false;
+    trigger2lvl1LRoom->sprite = ' ';
+    trigger2lvl1LRoom->level = 1;
+    trigger2lvl1LRoom->position.x = 29;
+    trigger2lvl1LRoom->position.y = 20;
+    trigger2lvl1LRoom->isVisible = false;
+    trigger2lvl1LRoom->tag = DEFAULT;
+    trigger2lvl1LRoom->OnUpdate = &TriggerUpdate;
+    sprintf(trigger2lvl1LRoom->name, "There is a big L in this room. I wonder what that could stand for?");
+        //hallway!
+    trigger3lvl1RightRoom = ADD_ENTITY();
+    trigger3lvl1RightRoom->isFile = false;
+    trigger3lvl1RightRoom->sprite = ' ';
+    trigger3lvl1RightRoom->level = 1;
+    trigger3lvl1RightRoom->position.x = 34;
+    trigger3lvl1RightRoom->position.y = 21;
+    trigger3lvl1RightRoom->isVisible = false;
+    trigger3lvl1RightRoom->tag = DEFAULT;
+    trigger3lvl1RightRoom->OnUpdate = &TriggerUpdate;
+    sprintf(trigger3lvl1RightRoom->name, "This hallway feels infinitely long! ...until it isn't");
 
-    //key 1 for door 1 in level 1
+    //DOORS AND KEYS
+        //key 1 for door 1 in level 1
     key1lvl1 = ADD_ENTITY();
     key1lvl1->isFile = true;
     key1lvl1->isJob = true;
@@ -62,7 +98,7 @@ void ENTITY_SETUP(){
     key1lvl1->OnUse = &UseKey;
     key1lvl1->useParam = key1lvl1;
     sprintf(key1lvl1->name, "1.key");
-
+        //first door the player shall open
     door1lvl1 = ADD_ENTITY();
     door1lvl1->isFile = false;
     door1lvl1->isJob = true;
@@ -74,8 +110,73 @@ void ENTITY_SETUP(){
     door1lvl1->tag = DOOR;
     sprintf(door1lvl1->name, "1.door");
 
-    //scrolls
+        //key 2 to the level south
+    key2lvl1 = ADD_ENTITY();
+    key2lvl1->isFile = true;
+    key2lvl1->isJob = true;
+    key2lvl1->isVisible = true;
+    key2lvl1->sprite = 'K';
+    key2lvl1->position.x = 46;
+    key2lvl1->position.y = 29;
+    key2lvl1->tag = PICKUP;
+    key2lvl1->level = 1;
+    key2lvl1->OnUse = &UseKey;
+    key2lvl1->useParam = key1lvl1;
+    sprintf(key2lvl1->name, "2.key");
 
+        //door 2 south level 1
+    door2lvl1 = ADD_ENTITY();
+    door2lvl1->isFile = false;
+    door2lvl1->isJob = true;
+    door2lvl1->sprite = '%';
+    door2lvl1->level = 1;
+    door2lvl1->position.x = 22;
+    door2lvl1->position.y = 9;
+    door2lvl1->isVisible = true;
+    door2lvl1->tag = DOOR;
+    sprintf(door2lvl1->name, "2.door");
+
+        //Door to secret room at the end of hallway
+    door5lvl1 = ADD_ENTITY();
+    door5lvl1->isFile = false;
+    door5lvl1->isJob = true;
+    door5lvl1->sprite = '%';
+    door5lvl1->level = 1;
+    door5lvl1->position.x = 55;
+    door5lvl1->position.y = 21;
+    door5lvl1->isVisible = true;
+    door5lvl1->tag = DOOR;
+    sprintf(door5lvl1->name, "5.door");
+    
+    //scrolls section for level 1
+
+        //awakening scroll
+    scroll1lvl1 = ADD_ENTITY();
+    scroll1lvl1->isFile = false;
+    scroll1lvl1->isJob = false;
+    scroll1lvl1->isVisible = true;
+    scroll1lvl1->level = 1;
+    scroll1lvl1->position.x = 32;
+    scroll1lvl1->position.y = 18;
+    scroll1lvl1->sprite = '@';
+    scroll1lvl1->tag = PICKUP;
+    scroll1lvl1->OnUse = &WriteToTerminalOutput;
+    scroll1lvl1->useParam = &scrollText1;
+    sprintf(scroll1lvl1->name, "awakening.scroll");
+
+        //linux scroll
+    scroll2lvl1 = ADD_ENTITY();
+    scroll2lvl1->isFile = false;
+    scroll2lvl1->isJob = false;
+    scroll2lvl1->isVisible = true;
+    scroll2lvl1->level = 1;
+    scroll2lvl1->position.x = 25;
+    scroll2lvl1->position.y = 25;
+    scroll2lvl1->sprite = '@';
+    scroll2lvl1->tag = PICKUP;
+    scroll2lvl1->OnUse = &WriteToTerminalOutput;
+    scroll2lvl1->useParam = &scrollText2;
+    sprintf(scroll2lvl1->name, "linux.scroll");
 
     //LEVEL 2
 
@@ -90,7 +191,7 @@ void ENTITY_SETUP(){
     entity2->tag = PICKUP;
     entity2->OnUse = &UseKey;
     entity2->useParam = entity2;
-    sprintf(entity2->name, "2.key");
+    sprintf(entity2->name, "99.key");
 
     entity3 = ADD_ENTITY();
     entity3->isFile = false;
@@ -101,20 +202,7 @@ void ENTITY_SETUP(){
     entity3->position.y = 26;
     entity3->isVisible = true;
     entity3->tag = DOOR;
-    sprintf(entity3->name, "2.door");
-
-    scroll1 = ADD_ENTITY();
-    scroll1->isFile = true;
-    scroll1->isJob = false;
-    scroll1->isVisible = true;
-    scroll1->level = 2;
-    scroll1->position.x = 10;
-    scroll1->position.y = 12;
-    scroll1->sprite = '@';
-    scroll1->tag = PICKUP;
-    scroll1->OnUse = &WriteToTerminalOutput;
-    scroll1->useParam = &scrollText1;
-    sprintf(scroll1->name, "basics.scroll");
+    sprintf(entity3->name, "99.door");
 
     lantern = ADD_ENTITY();
     lantern->isFile = true;
--- C_Code/LARK_Libraries/entity_manager.h
+++ C_Code/LARK_Libraries/entity_manager.h
@@ -14,15 +14,25 @@ enum TAGS{
 extern char scrollText1[512];
 
 extern Transform *ROOT;
+//level1
 extern Transform *trigger1lvl1WakeUp;
-extern Transform *door1lvl1;
+extern Transform *trigger2lvl1LRoom;
+extern Transform *trigger3lvl1RightRoom;
 extern Transform *key1lvl1;
+extern Transform *key2lvl1;
+extern Transform *door1lvl1;
+extern Transform *door2lvl1;
+extern Transform *door5lvl1;
+extern Transform *scroll1lvl1;
+extern Transform *scroll2lvl1;
+
 
 extern Transform *entity2;
 extern Transform *entity3;
-extern Transform *scroll1;
 extern Transform *lantern;
 extern Transform *chest;
+
+extern Transform *susScroll;
 
 //logs all entities
 void LOG_ENTITIES();
